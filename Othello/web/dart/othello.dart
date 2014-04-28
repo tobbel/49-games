b51ad16f82233ef079c0b6fe3a544246776e3fd4
Tobias Lilja <tobias.lilja@gmail.com>
2014-04-28 21:36:40 +0200

    some more drawing

--- Othello/web/dart/othello.dart
+++ Othello/web/dart/othello.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:html';
+import 'dart:math';
 
 void main()
 {
@@ -39,12 +40,56 @@ class Othello
   
   void draw(double time)
   {
-    var context = canvas.context2D;
+    CanvasRenderingContext2D context = canvas.context2D;
     context.clearRect(0, 0, canvasWidth, canvasHeight);
     
     // Green background
     context.fillStyle = backgroundColor;
     context.fillRect(0, 0, canvasWidth, canvasHeight);
     
+    // Board
+    int boardWidth = 512;
+    int boardHeight = 512;
+    int offset = 16;
+    for (int x = 0; x <= boardWidth; x += 64)
+    {
+        context.moveTo(0.5 + x + offset, offset);
+        context.lineTo(0.5 + x + offset, boardHeight + offset);
+    }
+    for (int y = 0; y <= boardHeight; y += 64) 
+    {
+      context.moveTo(offset, 0.5 + y + offset);
+      context.lineTo(boardWidth + offset, 0.5 + y + offset);
+    }
+
+    context.strokeStyle = "black";
+    context.stroke();
+    
+    // Pieces
+    offset += 32;
+    Random random = new Random();
+    for (int i = 0; i < 64; i++)
+    {
+      int x = offset + (64 * (i % 8));
+      int y = offset + (64 * (i ~/ 8));
+      if (random.nextBool())
+        drawCircle(x, y, 'black');
+      else
+        drawCircle(x, y, 'white');
+    }
+  }
+  
+  void drawCircle(int x, int y, String color)
+  {
+
+    int radius = 30;
+    CanvasRenderingContext2D context = canvas.context2D;
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * PI, false);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = '#003300';
+    context.stroke();
   }
 }
