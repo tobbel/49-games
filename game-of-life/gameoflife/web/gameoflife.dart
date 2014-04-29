3471cd8597895b5f6ba8e8fadcba3cad34a561d9
Tobias Lilja <tobias.lilja@gmail.com>
2014-04-29 22:04:18 +0200

    Added game of life shell
    For now, just the drawing

---
+++ game-of-life/gameoflife/web/gameoflife.dart
@@ -0,0 +1,104 @@
+import 'dart:html';
+import 'dart:async';
+
+void main()
+{
+  CanvasElement canvas = querySelector('#game');
+
+  // Create game async as microtask 
+  scheduleMicrotask(new GameOfLife(canvas).start);
+}
+
+class GameOfLife
+{
+  CanvasElement canvas;
+  CanvasRenderingContext2D context;
+  
+  final int gridSize = 10;
+  final int gridWidth = 50;
+  final int gridHeight = 50;
+  bool drawing = false;
+  int mouseX = 0;
+  int mouseY = 0;
+  
+  GameOfLife(this.canvas);
+  
+  List<bool> life = new List<bool>.filled(2500, false);
+  
+  void start()
+  {
+    context = canvas.context2D;
+    
+    // Lines
+    context.fillStyle = 'black';
+    for (int x = 1; x < canvas.width; x++)
+    {
+      context.fillRect(x * gridSize, 0, 1, canvas.height);
+      for (int y = 1; y < canvas.height; y++)
+      {
+        context.fillRect(0, y * gridSize, canvas.width, 1);
+      }
+    }
+    
+    // Listener
+    canvas.onMouseDown.listen(mouseDown);
+    
+    // Loop
+    window.animationFrame.then(update);
+  }
+  
+  void update(double dt)
+  {
+    
+  }
+  
+  Set<int> handledIndices = new Set<int>();
+  StreamSubscription mouseMoveStream;
+  StreamSubscription mouseUpStream;
+  void mouseDown(MouseEvent e)
+  {
+    handledIndices.clear();
+    drawing = true;
+    mouseX = e.client.x;
+    mouseY = e.client.y;
+    // Handle tile under mouse, bind move for other pieces
+    Rectangle rect = canvas.getBoundingClientRect();
+
+    int x = (e.client.x - rect.left).toInt();
+    int y = (e.client.y - rect.top).toInt();
+    int index = getTileUnderMouse(x, y);
+    
+    if (!handledIndices.contains(index))
+    {
+      x = index % 50;
+      y = index ~/ 50;
+
+      context.fillRect(gridSize * x, gridSize * y, gridSize, gridSize);
+    }
+    
+    handledIndices.add(index);
+    
+    mouseMoveStream = canvas.onMouseMove.listen(mouseMove);
+    mouseUpStream = canvas.onMouseUp.listen(mouseUp);
+  }
+  
+  int getTileUnderMouse(int x, int y)
+  {
+    final int gameX = x ~/ gridSize;
+    final int gameY = y ~/ gridSize;
+    return gameY * gridWidth + gameX;
+  }
+  
+  void mouseUp(MouseEvent e)
+  {
+    drawing = false;
+    mouseMoveStream.cancel();
+    mouseUpStream.cancel();
+  }
+  
+  void mouseMove(MouseEvent e)
+  {
+    mouseX = e.client.x;
+    mouseY = e.client.y;
+  }
+}
